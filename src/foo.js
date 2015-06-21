a44724cd55f4bec84e6fd603647f7312023555b8
John Shearer <john@ultranaut.com>
2015-06-21 14:51:37 -0400

    Export as module or set as global object, depending

--- src/foo.js
+++ src/foo.js
@@ -1,7 +1,21 @@
 'use strict';
 
-function foo(bar) {
-  return bar;
-}
+(function () {
 
-module.exports = foo;
+  var root = this;
+
+  function foo(bar) {
+    return bar;
+  }
+
+  // export as a Node module if we're in that environment
+  // otherwise set it as a global object (function, whatever)
+  if (typeof module !== 'undefined' &&
+      typeof module.exports !== 'undefined') {
+    module.exports = foo;
+  }
+  else {
+    root.foo = foo;
+  }
+
+}).call(this);
